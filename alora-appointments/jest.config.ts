d321e00159fc08f6cd44b40b33c310c11d6462f0
Mzwakhe Mokhatla <mokatla.mzwakhe@email.com>
2026-05-08 10:49:03 +0200

    husky sanity check

---
+++ alora-appointments/jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
+  testPathPattern: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{ts,tsx}',
+  ],
+  coverageThresholds: {
+    global: { branches: 70, functions: 70, lines: 70, statements: 70 }
+  }
+};
+
+export default config;
